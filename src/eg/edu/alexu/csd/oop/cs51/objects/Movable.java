163f958d266fb2d3e7ea2c424cdaf8e9132b9ccc
muhammad salah <msalah.29.10@gmail.com>
2018-12-17 17:04:16 +0200

    mmmm

--- src/eg/edu/alexu/csd/oop/cs51/objects/Movable.java
+++ src/eg/edu/alexu/csd/oop/cs51/objects/Movable.java
@@ -3,18 +3,36 @@ package eg.edu.alexu.csd.oop.cs51.objects;
 import java.awt.Color;
 
 import eg.edu.alexu.csd.oop.cs51.objects.states.State;
+import eg.edu.alexu.csd.oop.cs51.observer.Collision;
+import eg.edu.alexu.csd.oop.cs51.observer.Observer;
 
-public abstract class Movable extends AbstractObject{
+public abstract class Movable extends AbstractObject implements Observer{
+    private static final int OBJECT_WIDTH = 50;
+    private static final int OBJECT_HEIGHT = 50;
     private String name;
     private Color color;
     private State state;
     
     
-    public Movable(String path, String name, Color color) {
-        
+    public Movable(String path, String name, Color color, Collision collision) {
+        super(OBJECT_WIDTH, OBJECT_HEIGHT, path, false);
+        collision.addObserver(this);
+        this.name = name;
+        this.color = color;
     }
     
     public void setState(State state) {
         this.state = state;
     }
+    
+//    @Override
+//    public void update(String spot) {
+//        if(spot.equals("vanishGround")) {
+//           // state = ground state
+//            setVisible(false);
+//        } else if (spot.equals("rightStack")) {
+//            
+//        } else if (spot.equals(anObject))
+//        
+//    }
 }
