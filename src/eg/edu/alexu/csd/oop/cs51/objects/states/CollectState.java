4cd025683349cc67daa61ab7f14051efef2b7fc8
reham <rehamnaguib1@gmail.com>
2018-12-17 19:38:31 +0200

    collision detection and states update

--- src/eg/edu/alexu/csd/oop/cs51/objects/states/CollectState.java
+++ src/eg/edu/alexu/csd/oop/cs51/objects/states/CollectState.java
@@ -1,13 +1,26 @@
 package eg.edu.alexu.csd.oop.cs51.objects.states;
 
+import eg.edu.alexu.csd.oop.cs51.GameInfo;
 import eg.edu.alexu.csd.oop.cs51.objects.Movable;
+import eg.edu.alexu.csd.oop.cs51.objects.Skill;
 
 public class CollectState implements State {
+	private GameInfo gameInfo = GameInfo.getInstance();
+	String position;
+
+	public CollectState(String position) {
+		this.position = position;
+	}
 
 	@Override
 	public void doAction(Movable movable) {
-		movable.setState(this);	
-	}
-	
+		movable.setState(this);
 
+		if (position.equals("right")) {
+			gameInfo.addToRightStack(movable);
+		} else if (position.equals("left")) {
+			gameInfo.addToLeftStack(movable);
+		}
+
+	}
 }
--- src/eg/edu/alexu/csd/oop/cs51/objects/states/VanishState.java
+++ src/eg/edu/alexu/csd/oop/cs51/objects/states/VanishState.java
@@ -1,11 +1,13 @@
 package eg.edu.alexu.csd.oop.cs51.objects.states;
 
+import eg.edu.alexu.csd.oop.cs51.objects.Movable;
+
 public class VanishState implements State {
 
 	@Override
-	public void doAction() {
-		
-		
+	public void doAction(Movable movable) {
+		movable.setState(this);
+		movable.setVisible(false);
 	}
 
 }
