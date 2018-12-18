8d035b841b6feb71d2b6ea62bfb56b67672ae9db
muhammad salah <msalah.29.10@gmail.com>
2018-12-18 16:14:12 +0200

    fb

--- src/eg/edu/alexu/csd/oop/cs51/objects/Movable.java
+++ src/eg/edu/alexu/csd/oop/cs51/objects/Movable.java
@@ -10,34 +10,29 @@ public abstract class Movable extends AbstractObject implements Observer{
     private static final int OBJECT_WIDTH = 50;
     private static final int OBJECT_HEIGHT = 50;
     private String name;
-    private Color color;
     private State state;
 
     
-    public Movable(String path, String name, Color color, Collision collision) {
-        super(OBJECT_WIDTH, OBJECT_HEIGHT, path, false);
+    public Movable(String path, String name, Collision collision) {
+        super(OBJECT_WIDTH, OBJECT_HEIGHT, path, false, name);
         collision.addObserver(this);
         this.name = name;
-        this.color = color;
     }
     
     public Movable(Movable movable) {
         super(movable);
         this.name = new String(movable.name);
-        this.color = new Color(movable.color.getRGB());
         this.state = movable.state.clone();
     }
     
     public void setState(State state) {
         this.state = state;
     }
-    
-    public Color getColor() {
-        return color;
-    }
-    
     public String getName() {
         return name;
+    }
+    public void setName(String name) {
+        this.name = name;
     }
     public State getState() {
         return state;
