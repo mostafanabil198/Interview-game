43e96239902ba24ff38ecfddd1a583bf9877a87c
reham <rehamnaguib1@gmail.com>
2018-12-18 16:30:11 +0200

    youtube and whatsapp

---
+++ src/eg/edu/alexu/csd/oop/cs51/objects/skills/YoutubeSkill.java
@@ -0,0 +1,41 @@
+package eg.edu.alexu.csd.oop.cs51.objects.skills;
+
+import eg.edu.alexu.csd.oop.cs51.objects.Movable;
+import eg.edu.alexu.csd.oop.cs51.objects.Skill;
+import eg.edu.alexu.csd.oop.cs51.objects.states.CollectState;
+import eg.edu.alexu.csd.oop.cs51.objects.states.MovingState;
+import eg.edu.alexu.csd.oop.cs51.objects.states.VanishState;
+import eg.edu.alexu.csd.oop.cs51.observer.Collision;
+
+public class YoutubeSkill extends Movable implements Skill {
+    private static final String PATH = "res/youtube.png";
+
+    public YoutubeSkill(String name, Collision collision) {
+        super(PATH, name, collision);
+    }
+
+    public YoutubeSkill(YoutubeSkill youtubeSkill) {
+        super(youtubeSkill);
+    
+    }
+	@Override
+	public void update(String spot) {
+		 if(spot.equals("vanish")) {
+	           new VanishState().doAction(this);
+	        } else if (spot.equals("rightStack")) {
+	           new CollectState("right").doAction(this);
+	        } else if (spot.equals("leftStack")) {
+	           new CollectState("left").doAction(this);
+	        } else {
+	            MovingState state = (MovingState) getState();
+	            state.updatePosition(this);
+	         }
+		
+	}
+
+	@Override
+	public Movable clone() {
+		return new YoutubeSkill(this);
+	}
+
+}
