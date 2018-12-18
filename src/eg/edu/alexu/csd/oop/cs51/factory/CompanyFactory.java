fe73700713d2edecace5d5a2db7d9c7d0d706a94
alnashar <alnasharm778@gmail.com>
2018-12-18 18:08:57 +0200

    add factory

--- src/eg/edu/alexu/csd/oop/cs51/factory/CompanyFactory.java
+++ src/eg/edu/alexu/csd/oop/cs51/factory/CompanyFactory.java
@@ -1,5 +1,30 @@
 package eg.edu.alexu.csd.oop.cs51.factory;
 
-public class CompanyFactory {
+import java.util.ArrayList;
+
+import eg.edu.alexu.csd.oop.cs51.objects.Movable;
+
+public class CompanyFactory implements ICompanyFactory {
+	
+	private ArrayList<Class<Movable>> classes;
+	
+	 public CompanyFactory(ArrayList<Class<Movable>> classes) {
+		 this.classes = classes;
+		 
+	 }
+	
+
+	@Override
+	
+	public Movable createNewMovable(String companyName) throws InstantiationException, IllegalAccessException {
+		// TODO Auto-generated method stub
+		for(int i = 0;i< classes.size();i++) {
+			if(companyName.equals(classes.get(i).getSimpleName())) {
+				return classes.get(i).newInstance();
+			}
+			
+		}
+		return null;
+	}
 
 }
