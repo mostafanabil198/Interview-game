3a129727d540708222963a5c5ee7471680166e1a
HP <tarek.ahmed2009@yahoo.com>
2018-12-18 15:57:14 +0200

    flyweight and factory created

---
+++ src/eg/edu/alexu/csd/oop/cs51/factory/CompanyFactory.java
@@ -0,0 +1,5 @@
+package eg.edu.alexu.csd.oop.cs51.factory;
+
+public class CompanyFactory {
+
+}
---
+++ src/eg/edu/alexu/csd/oop/cs51/flyweight/FlyweightFactory.java
@@ -0,0 +1,5 @@
+package eg.edu.alexu.csd.oop.cs51.flyweight;
+
+public class FlyweightFactory {
+
+}
