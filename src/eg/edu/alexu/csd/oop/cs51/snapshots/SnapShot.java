a9e4e0a53a776584482ad63dd982081618deae40
mostafa nabil <mostafanabil198@gmail.com>
2018-12-19 18:20:39 +0200

    snapshot done

--- src/eg/edu/alexu/csd/oop/cs51/snapshots/SnapShot.java
+++ src/eg/edu/alexu/csd/oop/cs51/snapshots/SnapShot.java
@@ -6,6 +6,7 @@ import java.util.Stack;
 import eg.edu.alexu.csd.oop.cs51.GameInfo;
 import eg.edu.alexu.csd.oop.cs51.iterators.Collections;
 import eg.edu.alexu.csd.oop.cs51.iterators.IteratorI;
+import eg.edu.alexu.csd.oop.cs51.objects.AbstractObject;
 import eg.edu.alexu.csd.oop.cs51.objects.Movable;
 import eg.edu.alexu.csd.oop.cs51.objects.player.Interviewee;
 import eg.edu.alexu.csd.oop.cs51.observer.Collision;
@@ -16,7 +17,6 @@ import eg.edu.alexu.csd.oop.game.GameObject;
 public class SnapShot {
 	private int score;
 	private Collections<Movable> leftStack, rightStack;
-	private int time;
 	private int leftColorCounter;
 	private int rightColorCounter;
 	private int playerPositionX;
@@ -26,15 +26,13 @@ public class SnapShot {
 	private int leftHand;
 	private int rightHand;
 	private int numOfLives;
-	
-	
-	private Collections<GameObject> moving; 
-	private Collections<GameObject> control; 
+
+	private Collections<GameObject> moving;
+	private Collections<GameObject> control;
 
 	public SnapShot() {
 		score = GameInfo.getInstance().getScore();
 		fillStacksForSave();
-		time = GameInfo.getInstance().getTime();
 		leftColorCounter = GameInfo.getInstance().getLeftColorCounter();
 		rightColorCounter = GameInfo.getInstance().getRightColorCounter();
 		playerPositionX = GameInfo.getInstance().getPlayer().getX();
@@ -45,14 +43,14 @@ public class SnapShot {
 		leftHand = GameInfo.getInstance().getLeftHand();
 		rightHand = GameInfo.getInstance().getRightHand();
 		numOfLives = GameInfo.getInstance().getNumOfLives();
-		
-		
 
 	}
 
 	private void fillStacksForSave() {
 		leftStack.clear();
 		rightStack.clear();
+		moving.clear();
+		control.clear();
 		IteratorI iterator = GameInfo.getInstance().getLeftStack().createIterator();
 		while (iterator.hasNext()) {
 			leftStack.add((Movable) ((Movable) iterator.next()).clone());
@@ -60,6 +58,16 @@ public class SnapShot {
 		iterator = GameInfo.getInstance().getRightStack().createIterator();
 		while (iterator.hasNext()) {
 			rightStack.add((Movable) ((Movable) iterator.next()).clone());
+		}
+
+		iterator = GameInfo.getInstance().getMoving().createIterator();
+		while (iterator.hasNext()) {
+			moving.add((GameObject) ((AbstractObject) iterator.next()).clone());
+		}
+
+		iterator = GameInfo.getInstance().getControl().createIterator();
+		while (iterator.hasNext()) {
+			control.add((GameObject) ((AbstractObject) iterator.next()).clone());
 		}
 
 		/*
@@ -73,13 +81,24 @@ public class SnapShot {
 	private void fillStacksForLoad() {
 		GameInfo.getInstance().getLeftStack().clear();
 		GameInfo.getInstance().getRightStack().clear();
+		GameInfo.getInstance().getMoving().clear();
+		GameInfo.getInstance().getControl().clear();
 		IteratorI iterator = leftStack.createIterator();
 		while (iterator.hasNext()) {
-			GameInfo.getInstance().getLeftStack().add((Movable) ((Movable)iterator.next()).clone());
+			GameInfo.getInstance().getLeftStack().add((Movable) ((Movable) iterator.next()).clone());
 		}
 		iterator = rightStack.createIterator();
 		while (iterator.hasNext()) {
-			GameInfo.getInstance().getRightStack().add((Movable) ((Movable)iterator.next()).clone());
+			GameInfo.getInstance().getRightStack().add((Movable) ((Movable) iterator.next()).clone());
+		}
+
+		iterator = moving.createIterator();
+		while (iterator.hasNext()) {
+			GameInfo.getInstance().getMoving().add((GameObject) ((AbstractObject) iterator.next()).clone());
+		}
+		iterator = control.createIterator();
+		while (iterator.hasNext()) {
+			GameInfo.getInstance().getControl().add((GameObject) ((AbstractObject) iterator.next()).clone());
 		}
 	}
 
@@ -90,6 +109,13 @@ public class SnapShot {
 		GameInfo.getInstance().setRightColorCounter(rightColorCounter);
 		GameInfo.getInstance().getPlayer().setX(playerPositionX);
 		GameInfo.getInstance().getPlayer().setY(playerPositionY);
+
+		GameInfo.getInstance().setLeftHand(leftHand);
+		GameInfo.getInstance().setRightHand(rightHand);
+		GameInfo.getInstance().setLeftStackHeight(leftStackHeight);
+		GameInfo.getInstance().setRightStackHeight(rightStackHeight);
+		GameInfo.getInstance().setNumOfLives(numOfLives);
+
 	}
 
 }
