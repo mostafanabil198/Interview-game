0eb39cbaf50c367405a58a07ff4fe00da1e57d30
mostafa nabil <mostafanabil198@gmail.com>
2018-12-19 15:01:37 +0200

    add level 1 and 3

--- src/eg/edu/alexu/csd/oop/cs51/strategy/Level3Strategy.java
+++ src/eg/edu/alexu/csd/oop/cs51/strategy/Level3Strategy.java
@@ -1,6 +1,5 @@
 package eg.edu.alexu.csd.oop.cs51.strategy;
 
-import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -11,7 +10,7 @@ import eg.edu.alexu.csd.oop.cs51.GameInfo;
 import eg.edu.alexu.csd.oop.cs51.objects.Skill;
 import eg.edu.alexu.csd.oop.cs51.tasks.Task;
 
-public class Level1Strategy implements Strategy {
+public class Level3Strategy implements Strategy {
 
 	private List<Class<? extends Skill>> supported;
 	private List<String> allSkills;
@@ -19,7 +18,7 @@ public class Level1Strategy implements Strategy {
 	@Override
 	public Map<String, Object> doOperation() {
 		allSkills = GameInfo.getInstance().getAllSkills();
-		for (int i = 0; i < 3; i++) {
+		for (int i = 0; i < 5; i++) {
 			Collections.shuffle(supported);
 			Collections.shuffle(allSkills);
 			GameInfo.getInstance().addTask(
@@ -27,7 +26,7 @@ public class Level1Strategy implements Strategy {
 		}
 
 		Map<String, Object> map = new HashMap<>();
-		map.put("fireRate", 5);
+		map.put("fireRate", 2);
 		map.put("obaque", false);
 		return map;
 	}
