2858916f5a364585e7d0d3bb0ee771b58a913fab
muhammad salah <msalah.29.10@gmail.com>
2018-12-19 15:01:50 +0200

    constat objects

--- src/eg/edu/alexu/csd/oop/cs51/objects/constant/CanonObject.java
+++ src/eg/edu/alexu/csd/oop/cs51/objects/constant/CanonObject.java
@@ -2,18 +2,16 @@ package eg.edu.alexu.csd.oop.cs51.objects.constant;
 
 import eg.edu.alexu.csd.oop.cs51.objects.AbstractObject;
 
-public class CanonRight extends AbstractObject{
-    private static final String PATH = "res/canonRight.png";
+public class CanonObject extends AbstractObject{
+    private static final String PATH = "res/canons.png";
     private static final int WIDTH = 50;
     private static final int HEIGHT = 50;
 
-    public CanonRight() {
+    public CanonObject() {
         super(WIDTH, HEIGHT, PATH, true, "");
-        // TODO Auto-generated constructor stub
     }
     
-    public CanonRight(CanonRight canonRight) {
-        super(canonRight);
+    public CanonObject(CanonObject canonLeft) {
+        super(canonLeft);
     }
-
 }
--- src/eg/edu/alexu/csd/oop/cs51/objects/constant/CononLeft.java
+++
@@ -1,5 +0,0 @@
-package eg.edu.alexu.csd.oop.cs51.objects.constant;
-
-public class CononLeft {
-
-}
